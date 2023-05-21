b19e8b163d625345b1a7f59887f06463418b9e1e
Pratik <pendurkarpratik04@gmail.com>
2023-05-21 10:02:30 +0530

    Build a Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Grid, GridItem, Show } from '@chakra-ui/react';
+import NavBar from './components/NavBar';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
-}
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`, // Mobile
+        lg: `"nav nav" "aside  main"`, //1024px
+      }}
+    >
+      <GridItem area={'nav'}>
+        <NavBar />
+      </GridItem>
+      <Show above="lg">
+        <GridItem area={'aside'} bg={'blue'}>
+          aside
+        </GridItem>
+      </Show>
+      <GridItem area={'main'} bg={'yellow'}>
+        main
+      </GridItem>
+    </Grid>
+  );
+};
 
-export default App
+export default App;
