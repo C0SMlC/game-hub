69f28612f1f711f130331ecb4a18d6ee532e30c7
Pratik <pendurkarpratik04@gmail.com>
2023-05-29 10:52:47 +0530

    Displaying Platform Icons

--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -7,26 +7,40 @@ import {
   FaAndroid,
 } from 'react-icons/fa';
 
-import {MdPhoneIphone} from 'react-icons/md';
-import {SiNintendo} from 'react-icons/si';
-import {BsGlobe} from 'react-icons/bs';
+import { MdPhoneIphone } from 'react-icons/md';
+import { SiNintendo } from 'react-icons/si';
+import { BsGlobe } from 'react-icons/bs';
 import { Platform } from '../hooks/useGames';
-import { Text } from '@chakra-ui/react';
+import { HStack, Icon, Text } from '@chakra-ui/react';
+import { IconType } from 'react-icons/lib';
 
 interface props {
   platforms: Platform[];
 }
 
 const PlatformIconList = ({ platforms }: props) => {
+  const iconMap: { [key: string]: IconType } = {
+    pc: FaWindows,
+    playstation: FaPlaystation,
+    mac: FaApple,
+    xbox: FaXbox,
+    linux: FaLinux,
+    android: FaAndroid,
+    androidnew: MdPhoneIphone,
+    web: BsGlobe,
+    nintendo: SiNintendo,
+  };
 
-
-    
   return (
-    <>
+    <HStack marginY={'15px'}>
       {platforms.map((platform) => (
-        <Text>{platform.name}</Text>
+        <Icon
+          as={iconMap[platform.slug as keyof typeof iconMap]}
+          color="gray.500"
+          fontSize={'2xl'}
+        />
       ))}
-    </>
+    </HStack>
   );
 };
 
